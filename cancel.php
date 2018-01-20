638edb7fcc0c7cd72b4da7e8143a545e1b26fa80
Nikolay N Shmelev <aerograf@shmel.org>
2018-01-20 18:32:11 +0300

    Del

--- cancel.php
+++
@@ -1,4 +0,0 @@
-<?php
-include("../../mainfile.php");
-redirect_header(XOOPS_URL . '/index.php',5,'Your payment for the torrent was canceled. You are being redirected to the home page.');
-
--- language/english/blocks.php
+++
@@ -1,7 +0,0 @@
-<?
-
-// Blocks
-define("_MB_XTORRENT_DISP","Afficher");
-define("_MB_XTORRENT_FILES","Fichier");
-define("_MB_XTORRENT_CHARS","Longueur du titre");
-define("_MB_XTORRENT_LENGTH"," Caractères");
